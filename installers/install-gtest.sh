47db30aec7580e35fbc6908e80ef674f5deec141
SAVELIY BAKTURIN <zavierwaffle@gmail.com>
2024-06-26 08:40:57 +0000

    * Simplified building GoogleTest.

--- installers/install-gtest.sh
+++ installers/install-gtest.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 git clone https://github.com/google/googletest.git -b v1.14.0 gtest-src/
-mkdir gtest-src/BUILD/ && cd gtest-src/BUILD || exit 1
-CC=${COMPILER_NAME}-${COMPILER_VERSION} CXX=${COMPILER_NAMEXX}-${COMPILER_VERSION} cmake .. -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=../../googletest
+cd gtest-src/ && CC=${COMPILER_NAME}-${COMPILER_VERSION} CXX=${COMPILER_NAMEXX}-${COMPILER_VERSION} cmake . -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=../googletest
 make && make install
-cd ../../ && rm -rf gtest-src/
+cd .. && rm -rf gtest-src/
