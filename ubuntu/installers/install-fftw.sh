f95394e192a80c8d615c179250c9a4ed4a76b97f
SAVELIY BAKTURIN <zavierwaffle@gmail.com>
2024-08-03 14:18:35 +0300

    * Make FFTW building sources separately.

--- ubuntu/installers/install-fftw.sh
+++ ubuntu/installers/install-fftw.sh
@@ -2,11 +2,13 @@
 
 set -euo pipefail
 
-FFTW_VERSION="3.3.10"
+FFTW_VERSION='3.3.10'
 
-FFTW_URL="https://www.fftw.org"
+FFTW_URL='https://www.fftw.org'
 FFTW_SRC="fftw-${FFTW_VERSION}"
 FFTW_TAR="${FFTW_SRC}.tar.gz"
+
+BUILDDIR='BUILD'
 
 # Download and extract FFTW sources.
 wget "${FFTW_URL}"/"${FFTW_TAR}"
@@ -16,8 +18,12 @@ rm "${FFTW_TAR}"
 # Set working directory to FFTW sources.
 pushd "${FFTW_SRC}"
 
-# Build FFTW.
-cmake . -D CMAKE_INSTALL_PREFIX="${EDUCONTAINER_FFTW}" -D ENABLE_FLOAT=ON -D ENABLE_LONG_DOUBLE=ON -D ENABLE_QUAD_PRECISION=ON
+# Set working directory to temporary built sources.
+mkdir "${BUILDDIR}"
+pushd "${BUILDDIR}"
+
+# Build FFTW (Single precision).
+cmake .. -D CMAKE_INSTALL_PREFIX="${EDUCONTAINER_FFTW}" -D ENABLE_FLOAT=ON
 make -j"$(nproc --all)"
 
 # If BUILD_EXTRAS_RUN_TESTS == true, then we should test FFTW.
@@ -30,10 +36,31 @@ then
   ./bench --speed 512x1024
 fi
 
-# Install FFTW.
+# Install FFTW (Single precision).
+make install
+
+# Clean built.
+rm -rf *
+
+# Build FFTW (Double precision).
+cmake .. -D CMAKE_INSTALL_PREFIX="${EDUCONTAINER_FFTW}" -D ENABLE_DOUBLE=ON
+make -j"$(nproc --all)"
+
+# If BUILD_EXTRAS_RUN_TESTS == true, then we should test FFTW.
+if $BUILD_EXTRAS_RUN_TESTS
+then
+  ## Test FFTW.
+  ./bench --verify ib8192
+  ./bench --speed ib8192
+  ./bench --verify 512x1024
+  ./bench --speed 512x1024
+fi
+
+# Install FFTW (Double precision).
 make install
 
 # Go back.
+popd
 popd
 
 # Remove sources.
